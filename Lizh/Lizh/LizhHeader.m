5450c62c01eae2a314105f30dcbd76128b17226c
lizh <104335352+Spacebiblizh@users.noreply.github.com>
2022-11-29 15:57:51 +0800

    77

---
+++ Lizh/Lizh/LizhHeader.m
@@ -0,0 +1,17 @@
+//
+//  LizhHeader.m
+//  Lizh
+//
+//  Created by lizh on 2022/11/29.
+//
+
+#import "LizhHeader.h"
+
+@implementation LizhHeader
+
++ (NSString *) s{
+    
+    return  @"ddd";
+}
+
+@end
---
+++ Lizh/Products/Lizh.framework/Headers/Lizh.h
@@ -0,0 +1,19 @@
+//
+//  Lizh.h
+//  Lizh
+//
+//  Created by lizh on 2022/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Lizh.
+FOUNDATION_EXPORT double LizhVersionNumber;
+
+//! Project version string for Lizh.
+FOUNDATION_EXPORT const unsigned char LizhVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lizh/PublicHeader.h>
+
+
+#import <Lizh/LizhHeader.h>
